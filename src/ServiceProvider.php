0e2546c0986d87cebd873b276f8f37a16037e352
ersaazis <eam24maret@gmail.com>
2020-10-28 21:37:06 +0700

    first commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ersaazis\CrudApi;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Container\Container;
+use ersaazis\CrudApi\Console\Commands\ApiCrudMaker;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider 
+{
+
+    public function boot(Factory $view, Dispatcher $events, Repository $config) 
+    {     
+        $this->loadTranslations();
+
+        $this->registerCommands();
+    }
+
+    private function loadTranslations()
+    {
+        $translationsPath = $this->packagePath('resources/lang');
+
+        $this->loadTranslationsFrom($translationsPath, 'crud-api');
+
+        $this->publishes([
+            $translationsPath => resource_path('lang/vendor/crud-api'),
+        ], 'translations');
+    }
+
+    private function packagePath($path)
+    {
+        return __DIR__."/../$path";
+    }
+
+    private function registerCommands()
+    {
+        $this->commands(ApiCrudMaker::class);
+    }
+
+}
